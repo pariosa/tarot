0aa2d19ed5553f59b8a4ea79a88b2d1efacb7ee6
Peter Ariosa <ariosa@gmail.com>
2025-08-08 01:17:02 -0400

    add entity scanner to java layer, properly componentize jwtfilter layer

--- src/main/java/com/mercy/tarot/TarotApplication.java
+++ src/main/java/com/mercy/tarot/TarotApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.mercy.tarot.runners.DatabaseSeederRunner;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.mercy.tarot") // Ensure this covers your filter package
 @EnableJpaRepositories(basePackages = {
 		"com.mercy.tarot.repositories",
 		"com.mercy.tarot.models",
--- src/main/java/com/mercy/tarot/security/JwtAuthenticationFilter.java
+++ src/main/java/com/mercy/tarot/security/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.mercy.tarot.service.JwtTokenService;
@@ -13,6 +14,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenService jwtTokenService;
