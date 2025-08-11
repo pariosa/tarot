443bb59075caac4ab7961c6b4da77d09e2d046f4
Peter Ariosa <ariosa@gmail.com>
2025-08-11 13:43:22 -0400

    fix tailwind implementation

--- tarot-frontend/src/main.tsx
+++ tarot-frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router'
+import './App.css'
 import { AuthProvider } from './hooks/useAuth'
 import './index.css'
 import { router } from './routes/Router'
